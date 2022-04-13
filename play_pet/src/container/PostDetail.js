e2648b4088c8ff391563e5362de95158fa915429
SeonsikYun <42165194+suns2131@users.noreply.github.com>
2022-04-13 14:43:41 +0900

    connect test1

--- play_pet/src/container/PostDetail.js
+++ play_pet/src/container/PostDetail.js
@@ -1,18 +1,29 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Post from "../components/Post";
 import CommentList from "../components/CommentList";
 import CommentWrite from "../components/CommentWrite";
 import Headers from "../shared/Headers";
 import Container from '@mui/material/Container';
+import {actionCreators as PostActions} from '../redux/modules/Post_savestate'
+import { useDispatch, useSelector } from "react-redux";
 
 const PostDetail = (props) => {
     const data =props.match.params.postid;
+    const dispatch = useDispatch();
+    const post_data = useSelector((state) => state.post.list);
+    console.log(post_data);
+    
     console.log(data)
+
+    useEffect(() => {
+        dispatch(PostActions.getPostID(data));
+    },[])
+
     return (
         <React.Fragment>
             <Headers />
             <Container >
-                <Post/>
+                <Post post_data = {post_data}/>
                 <CommentWrite postid = {data}/>
                 <CommentList postid = {data}/>
             </Container>
--- play_pet/src/container/Posting.js
+++ play_pet/src/container/Posting.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import Container from '@mui/material/Container';
 import Post_content from '../componets/Post_content';
+import Headers from '../shared/Headers';
 
 const Posting = () => {
     return (
-        <Container maxWidth="lg">
-            <Post_content/>
-        </Container>
+        <div>
+            <Headers />
+            <Container maxWidth="lg">
+                <Post_content/>
+            </Container>
+        </div>
+        
     );
 }
 
